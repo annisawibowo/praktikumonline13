a35af7d1ce1e72c7f965fa91dd5f701f8d1de9b4
annisawibowo <annisa.raudya98@gmail.com>
2020-04-30 16:10:08 +0700

    Tugas

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridItemDecoration.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridItemDecoration.kt
@@ -10,11 +10,9 @@ import androidx.recyclerview.widget.RecyclerView
  */
 class ProductGridItemDecoration(private val largePadding: Int, private val smallPadding: Int) : RecyclerView.ItemDecoration() {
 
-    override fun getItemOffsets(outRect: Rect, view: View,
-                                parent: RecyclerView, state: RecyclerView.State) {
+    override fun getItemOffsets(outRect: Rect, itemPosition: Int, parent: RecyclerView)
+    {
         outRect.left = smallPadding
-        outRect.right = smallPadding
-        outRect.top = largePadding
-        outRect.bottom = largePadding
+        outRect.right = largePadding
     }
 }
